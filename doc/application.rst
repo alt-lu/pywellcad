eabf8371caa6ec26610435e9483350191bbf6b48
Matthew Schubert <matthew.schubert@alt.lu>
2021-12-10 07:46:25 +0000

    Tidy COM interface for Application (#25)
    This is a PR with some bulk changes:
    * Pull method flagging into constructor. I think it may be wise to actually create a smart dispatch wrapper that takes a list into the constructor to do this for other classes.
    * Updated default/optional parameter handling. It turns out `win32com` safely handles sending `None` as a parameter, and then the default is used.
    * Changed some whitespace to be in line with PEP8.
    * Removed "smarts" from all method calls, such as index checking and the like.
    
    Reviewed-on: https://git.alt.lu/alt/pywellcad/pulls/25
    Co-authored-by: Matthew Schubert <matthew.schubert@alt.lu>
    Co-committed-by: Matthew Schubert <matthew.schubert@alt.lu>

--- doc/application.rst
+++ doc/application.rst
@@ -1,14 +1,6 @@
 Application
 ===========
 
-The Application class is the core class that is used to interact with WellCAD. In general, it is the only one you should construct directly::
-
-   import wellcad.com
-
-   app = wellcad.com.Application()
-
-All interaction with borehole documents, logs, etc, should be done through this initial :ref:`Application` instance.
-
 .. autoclass:: wellcad.com.Application
    :members:
    :undoc-members:
--- wellcad/com/_application.py
+++ wellcad/com/_application.py
@@ -2,63 +2,75 @@ from win32com.client import Dispatch
 from ._borehole import Borehole
 
 class Application:
+    """The core class used to interact with WellCAD via its COM API.
 
+    In general, it is the only class you should instantiate directly. All
+    interaction with borehole documents, logs, etc, should be done through
+    this initial ``Application`` instance.
+
+    Instantiating an ``Application`` will automatically open a WellCAD instance
+    if there is not one already open, or connect to an existing WellCAD
+    instance if there is one open.
+
+    Before using the COM API, ensure WellCAD has been started at least once by
+    an administrator in order to register it in the computer registry.
+
+    Example
+    -------
+    >>> import wellcad.com
+    >>> app = wellcad.com.Application()
+    >>> app.show_window()
+    True
+    >>> app.new_borehole()
+    <wellcad.com._borehole.Borehole object at 0x000001D6973FBD30>
+    """
+
+    _DISPATCH_METHODS = ("ShowWindow", "NewBorehole", "OpenBorehole",
+        "GetBorehole", "GetActiveBorehole", "FileImport", "MultiFileImport")
+    
     def __init__(self):
-        """Creates the WellCAD object. 
-        
-        Ensure WellCAD has been started at least once by an
-        administrator in order to register it in the computer registry.
-        """
-
         self._dispatch = Dispatch("WellCAD.Application")
+        for method_name in self._DISPATCH_METHODS:
+            self._dispatch._FlagAsMethod(method_name)
         
-
     def show_window(self):
         """Attempts to display the WellCAD workspace on screen.
         
         Returns
-        =======
+        -------
         bool
             Whether the window was successfully shown.
         """
-        self._dispatch._FlagAsMethod("ShowWindow")
         return self._dispatch.ShowWindow()
         
-
     def minimize_window(self):
         """Collapses the application window to an icon."""
         self._dispatch.MinimizeWindow()
-        
-        
+    
     def maximize_window(self):
         """Extends the application window to full screen."""
         self._dispatch.MaximizeWindow()
-        
-        
+    
     def cascade(self):
         """Cascades all borehole document windows (unless tabbed)."""
         self._dispatch.Cascade()
-        
-
+    
     def tile_horizontally(self):
         """Arranges document windows horizontally (unless tabbed)."""
         self._dispatch.TileHorizontally()
-        
     
     def tile_vertically(self):
         """Arranges document windows vertically (unless tabbed)."""
         self._dispatch.TileVertically()
-        
-
-    def new_borehole(self, template = ""):
+    
+    def new_borehole(self, template=None):
         """Creates a new borehole document object.
 
-        The new borehole object is blank by default, unless a template
-        file is provided.
+        The new borehole object is blank unless a template file is provided.
         
         Parameters
         ----------
-        template : str
+        template : str, optional
             An optional path to a borehole document template (.wdt).
             If provided, the new borehole will be created using the
             template.
@@ -68,56 +80,47 @@ class Application:
         Borehole
             The new borehole document.
         """
-
-        self._dispatch._FlagAsMethod("NewBorehole")
-        obBhole = self._dispatch.NewBorehole(template)
-        return Borehole(obBhole)
+        return Borehole(self._dispatch.NewBorehole(template))
 
         
-    def open_borehole(self, path = ""):
+    def open_borehole(self, path=None):
         """Opens a WellCAD borehole document file (.wcl file).
         
         Parameters
         ----------
-        path : str
-            The file path to the WellCAD borehole document file to open.
-            If an empty string is provided, the user will be prompted
-            to select a file using a standard File Open dialog box.
+        path : str, optional
+            The file path to the WellCAD borehole document file to open. If no
+            file path is provided, the user will be prompted to select a file
+            using a standard File Open dialog box.
         
         Returns
         -------
-        Borehole
-            The opened borehole document.
+        Borehole or None
+            The opened borehole document, or ``None`` if opening the file
+            failed.
         """
-
-        self._dispatch._FlagAsMethod("OpenBorehole")
-        obBhole = self._dispatch.OpenBorehole(path)
-        return Borehole(obBhole)
+        return Borehole(self._dispatch.OpenBorehole(path))
         
 
-    def get_borehole(self, index = -1):
+    def get_borehole(self, index=None):
         """Gets an existing borehole document by index.
         
         Gets a borehole document object according to the zero-based index of
-        the document open in WellCAD. If no parameter is provided, the most
-        recent borehole document opened in WellCAD will be returned.
+        the document open in WellCAD.
         
         Parameters
         ----------
-        index : (str)
-            An optional zero based index of the borehole document.
+        index : int, optional
+            A zero based index of the borehole document. If no index is
+            provided, the index of the most recently used document is used.
         
         Returns
         -------
-        Borehole
-            The borehole document object with the specified index.
+        Borehole or None
+            The borehole document object with the specified index, or ``None``
+            if the index is out of bounds.
         """
-
-        if index == -1:
-            index = self._dispatch.NbOfDocuments - 1
-        self._dispatch._FlagAsMethod("GetBorehole")
-        obBhole = self._dispatch.GetBorehole(index)
-        return Borehole(obBhole)
+        return Borehole(self._dispatch.GetBorehole(index))
         
         
     def get_active_borehole(self):
@@ -125,15 +128,14 @@ class Application:
                    
         Returns
         -------
-        Borehole
-            The active borehole document object
+        Borehole or None
+            The current active borehole document object, or ``None`` if no
+            document has focus.
         """
-
-        obBhole = self._dispatch.GetActiveBorehole
-        return Borehole(obBhole)
+        return Borehole(self._dispatch.GetActiveBorehole())
         
         
-    def close_borehole(self, prompt_for_saving = True, index = -1):
+    def close_borehole(self, prompt_for_saving=None, index=None):
         """Closes a specific borehole document.
         
         The borehole document that is closed can be specified by an index,
@@ -142,29 +144,25 @@ class Application:
         
         Parameters
         ----------
-        prompt_for_saving : bool
+        prompt_for_saving : bool, optional
             Whether or not to prompt the user to save the borehole document.
-        index : int
-            The (zero-based) index of the document to close. If not provided,
-            the most recently opened document will be closed.
+        index : int or str, optional
+            The (zero-based) index of the document to close, or the document
+            name. If not provided, the most recently opened document will be
+            closed.
         """
-
-        if index == -1:
-            index = self._dispatch.NbOfDocuments - 1
         self._dispatch.CloseBorehole(prompt_for_saving, index)
-        
-        
+    
     @property
     def borehole_count(self):
-        """Returns the number of borehole documents open in WellCAD."""
+        """The number of borehole documents open in WellCAD."""
         return self._dispatch.NbOfDocuments
-        
     
     def file_import(self,
-                    file_name = "",
-                    prompt_user = True,
-                    config_file = "",
-                    log_file = ""):
+                    file_name=None,
+                    prompt_user=None,
+                    config_file=None,
+                    log_file=None):
         """Imports the specified file into a new borehole document.
         
         Allows import of TFD, LAS, DLIS, TXT, CSV and other file
@@ -175,14 +173,14 @@ class Application:
 
         Parameters
         ----------
-        file_name : str
+        file_name : str, optional
             The path of the file to import.
-        prompt_user : bool
+        prompt_user : bool, optional
             Whether to display an import dialog box to allow the user
             to specify import settings.
-        config_file : str
+        config_file : str, optional
             Path and filename of the configuration file or parameter string.
-        log_file : str
+        log_file : str, optional
             Path and name of the file to log error messages.
         
         Returns
@@ -190,19 +188,16 @@ class Application:
         Borehole
             A new borehole document containing the imported data.
         """
-
-        self._dispatch._FlagAsMethod("FileImport")
-        obBhole = self._dispatch.FileImport(file_name,
-                                           prompt_user,
-                                           config_file,
-                                           log_file)
-        return Borehole(obBhole)
+        return Borehole(self._dispatch.FileImport(file_name,
+                                                  prompt_user,
+                                                  config_file,
+                                                  log_file))
         
-        
-    def multi_file_import(self, file_name = "",
-                          prompt_user = True,
-                          config_file = "",
-                          log_file = ""):
+    def multi_file_import(self,
+                          file_name=None,
+                          prompt_user=None,
+                          config_file=None,
+                          log_file=None):
         """Creates a single borehole document from all specified files.
 
         Allows import of multiple TFD, LAS, DLIS, TXT, CSV and other
@@ -214,13 +209,13 @@ class Application:
 
         Parameters
         ----------
-        file_name : str
+        file_name : str, optional
             A comma separated list of input files (path and file name). 
-        prompt_user : str
+        prompt_user : bool, optional
             Boolean to display the import dialog boxes.
-        config_file : str
+        config_file : str, optional
             Path and filename of the configuration file or parameter string.
-        log_file : str
+        log_file : str, optional
             Path and name of the file to log error messages.
         
         Returns
@@ -229,22 +224,18 @@ class Application:
             A newly created borehole document containing the data from the
             imported files.
         """
-
-        self._dispatch._FlagAsMethod("MultiFileImport")
-        obBhole = self._dispatch.MultiFileImport(file_name,
-                                                prompt_user,
-                                                config_file,
-                                                log_file)
-        return Borehole(obBhole)
-        
-        
-    def quit(self, prompt_for_saving = True):
+        return Borehole(self._dispatch.MultiFileImport(file_name,
+                                                       prompt_user,
+                                                       config_file,
+                                                       log_file))       
+    
+    def quit(self, prompt_for_saving=None):
         """Closes all borehole documents and exits WellCAD.
         
         Parameters
         ----------
-        prompt_for_saving : bool
+        prompt_for_saving : bool, optional
             Whether to display the Save As dialog box for unsaved documents.
+            Default behaviour is to prompt the user.
         """
-
         self._dispatch.Quit(prompt_for_saving)
