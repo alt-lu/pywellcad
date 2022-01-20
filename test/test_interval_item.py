5769cda91e6002541caecc68a18bccf3f408bc22
Arnaud Cayrol <arnaud.cayrol@alt.lu>
2022-01-20 08:42:51 +0000

    Improve testing of IntervalItem (#59)
    Testing should be done using data from sample wellcad files.
    
    Reviewed-on: https://git.alt.lu/alt/pywellcad/pulls/59
    Co-authored-by: Arnaud Cayrol <arnaud.cayrol@alt.lu>
    Co-committed-by: Arnaud Cayrol <arnaud.cayrol@alt.lu>

--- test/test_interval_item.py
+++ test/test_interval_item.py
@@ -2,28 +2,30 @@ import unittest
 import pathlib
 import wellcad.com
 from ._extra_asserts import ExtraAsserts
+from ._sample_path import SamplePath
 
 
-class TestIntervalItem(unittest.TestCase, ExtraAsserts):
+class TestIntervalItem(unittest.TestCase, ExtraAsserts, SamplePath):
     @classmethod
     def setUpClass(cls):
         cls.app = wellcad.com.Application()
-        cls.borehole = cls.app.new_borehole()
-        cls.log = cls.borehole.insert_new_log(13)
-        cls.item = cls.log.insert_new_interval_item(10.0, 12.0, 25.2)
+        cls.sample_path = cls._find_sample_path()
+        cls.borehole = cls.app.open_borehole(str(cls.sample_path / "Geotech Plot.WCL"))
+        cls.log = cls.borehole.log("Sampling")
+        cls.item = cls.log.interval_item(0)
 
     @classmethod
     def tearDownClass(cls):
         cls.app.quit(False)
 
     def test_top_depth(self):
-        self.assertAlmostEqual(self.item.top_depth, 10.0, 3)
+        self.assertAttrEqual(self.item, "top_depth", 0.012770021334290504)
 
     def test_bottom_depth(self):
-        self.assertAlmostEqual(self.item.bottom_depth, 12.0, 3)
+        self.assertAttrEqual(self.item, "bottom_depth", 2.014082670211792)
 
     def test_set_value(self):
-        self.assertAlmostEqual(self.item.value, 25.2, 3)
+        self.assertAttrEqual(self.item, "value", 1.6380952596664429)
         self.assertAttrAlmostChange(self.item, 'value', 30.4, 3)
 
 
