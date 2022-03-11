f4ef8a118913889f722e3beabb77276626603564
Arnaud Cayrol <arnaud.cayrol@alt.lu>
2022-03-11 11:10:12 +0100

    LogData is a subclass of numpy array

--- wellcad/log_data/_log_data.py
+++ wellcad/log_data/_log_data.py
@@ -1,6 +1,15 @@
-from . import com
+from wellcad.com._log import Log
+import numpy as np
 
 
-class LogData():
-    def __init__(self, name):
-        self.name = name
+class LogData(np.ndarray):
+    def __new__(cls, data_table: Log.data_table):
+        obj = np.asarray(data_table[1:][1:]).view(cls)
+        obj.column_labels = np.array(data_table[0][1:], dtype=str)
+        obj.depth_or_time = np.array(data_table[1:][0], dtype=float)
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None: return
+        self.column_labels = getattr(obj, 'column_labels', None)
+        self.depth_or_time = getattr(obj, 'depth_or_time', None)
