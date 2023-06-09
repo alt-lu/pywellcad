8d64a8818ac3eb035f92a67c22cceff3988b6461
Arnaud Cayrol <arnaud.cayrol@alt.lu>
2023-06-09 10:32:52 +0200

    Added pywintypes to the list of "ignored" modules during documentation build

--- doc/conf.py
+++ doc/conf.py
@@ -37,7 +37,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ["win32com"]
+autodoc_mock_imports = ["win32com", "pywintypes"]
 
 # -- Options for HTML output -------------------------------------------------
 
