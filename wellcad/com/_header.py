6f78cbe84b7f2941646cd9035e341d1326b7c7d8
ALT\tanguy <tanguy.holl@alt.lu>
2026-06-19 09:02:10 +0200

    Automation update for header management

--- wellcad/com/_header.py
+++ wellcad/com/_header.py
@@ -119,3 +119,71 @@ class Header(DispatchWrapper):
         """
 
         self._dispatch.AllowExportTrailer(index, enable, password)
+
+    @property
+    def nb_of_header_forms(self):
+        """int: The number of header forms."""
+        return self._dispatch.NbOfHeaderForms
+
+    @property
+    def nb_of_trailer_forms(self):
+        """int: The number of trailer forms."""
+        return self._dispatch.NbOfTrailerForms
+
+    def add_header_form(self, filename):
+        """Adds a new header form and returns its ID.
+
+        Parameters
+        ----------
+        filename : string
+            Name and path of the header file.
+
+        Returns
+        -------
+        int
+            The ID of the last header form.
+        """
+        return self._dispatch.AddHeaderForm(filename)
+
+    def add_trailer_form(self, filename):
+        """Adds a new trailer form and returns its ID.
+
+        Parameters
+        ----------
+        filename : string
+            Name and path of the trailer form.
+
+        Returns
+        -------
+        int
+            The ID of the last trailer form.
+        """
+        return self._dispatch.AddTrailerForm(filename)
+
+    def delete_header_form(self, index):
+        """Delete the header form positioned at the index.
+
+        Parameters
+        ----------
+        index : index
+            Index of the header form to delete.
+        """
+        self._dispatch.DeleteHeaderForm(index)
+
+    def delete_trailer_form(self, filename):
+        """Delete the trailer form positioned at the index.
+
+        Parameters
+        ----------
+        index : index
+            Index of the trailer form to delete.
+        """
+        self._dispatch.DeleteTrailerForm(filename)
+
+    def delete_all_header_forms(self):
+        """Delete all header forms."""
+        self._dispatch.DeleteAllHeaderForms()
+
+    def delete_all_trailer_forms(self):
+        """Delete all trailer forms."""
+        self._dispatch.DeleteAllTrailerForms()
