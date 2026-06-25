582bc764490883441a29005f47f4597fcd922d01
ALT\tanguy <tanguy.holl@alt.lu>
2026-06-25 13:15:04 +0200

    Implementation and test of the tensile strength process

--- wellcad/com/_borehole.py
+++ wellcad/com/_borehole.py
@@ -22,7 +22,7 @@ class Borehole(DispatchWrapper):
                          "ConvertLogTo", "FilterLog", "ResampleLog", "InterpolateLog", "ElogCorrection",
                          "NMRFluidVolumes", "ROPAverage", "SharpenRGBLog", "RetinexFilterRGBLog", 
                          "Transmissivity", "ShearWaveVelocity", "EllipseFitting", "BreakoutAutoPick",
-                         "CreateLinkedLog")
+                         "CreateLinkedLog", "TensileStrength")
 
     @property
     def name(self):
@@ -4381,3 +4381,39 @@ class Borehole(DispatchWrapper):
             log from.
         """
         return Log(self._dispatch.CreateLinkedLog(log))
+
+    def tensile_strength(self, config=None):
+        """Computes the tensile strength using the method and parameters specified in the configuration file.
+
+        Parameters
+        ----------
+        config : str, optional
+            Path to a configuration file or a parameter string. The
+            configuration file can contain the following options:
+
+             .. code-block:: ini
+
+            [TensileStrength]
+            ; method : one of : 0 (constant), 1 (Khandelwal, for all types of rocks), 2 (Singh, for all types of rocks), 3 (Li, for granites), 4 (Zheng, for sedimentary rocks); default = 0
+            ; OutputUnit : one of : psi, MPa, Mpsi, GPa; default : psi
+            ; Constant : log or value
+            ; ConstantUnit : one of : psi, MPa, Mpsi, GPa; default : psi
+            ; PSlowness : log or value
+            ; PSlownessUnit : one of us/ft, us/m, ft/us, m/s
+            ; TopDepth and BottomDepth in meters. If equal, the full depth range is considered
+
+            Method = 1
+            OutputUnit = one of GPa, MPa, Pa, bar, atm, psi, Mpsi, N/m2
+            Constant = 100
+            ConstantUnit = one of GPa, MPa, Pa, bar, atm, psi, Mpsi, N/m2
+            PSlowness = P Slowness
+            PSlownessUnit = one of m/s, ft/s, km/s, us/ft, us/m, ft/us, uSec/ft
+            TopDepth = 100
+            BottomDepth = 135
+
+        Returns
+        -------
+        Log
+            A log containing the tensile strength.
+        """
+        return Log(self._dispatch.TensileStrength(config))
